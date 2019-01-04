be39b7c11828c634f19ab79a1326d2b6516676dc
Shaik Khader Basha <shaik.khader.basha@philips.com>
2019-01-04 13:24:29 +0530

    BaseActivity added

---
+++ app/src/main/java/khader/shaik/io/openweatherapp/BaseActivity.java
@@ -0,0 +1,21 @@
+/*
+ *  Copyright (c) $year. Philips India Ltd.
+ *  All rights reserved. Reproduction in whole or in part is prohibited
+ *  without the written consent of the copyright holder.
+ *
+ */
+
+package khader.shaik.io.openweatherapp;
+
+/**
+ *  Project           : OpenWeatherDemo
+ *  File Name         : BaseActivity
+ *  Description       :
+ *  Revision History  : version 1
+ *  Date              : 1/4/19
+ *  Original author   : Shaik Khader Basha
+ *  Description       : Initial version
+ *  
+ */
+public class BaseActivity {
+}
